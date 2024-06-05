ab6fe157ce60f692c5466f12c7bceeb8ad41c483
Joey Huang <yeeeshiuan.huang@gmail.com>
2024-06-05 15:42:15 +0800

    Finished the route and make sure the response is json type

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Middleware\ForceJsonResponse;
+
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -11,7 +13,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->append(ForceJsonResponse::class);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,19 @@
 <?php
 
+use App\Http\Requests\CurrencyExchangeRequest;
+use App\Services\CurrencyExchangeServiceInterface;
+
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('check', function (CurrencyExchangeRequest $request) {
+    $newAmount = app(CurrencyExchangeServiceInterface::class)->exchange(
+        $request->input('source'),
+        $request->input('target'),
+        $request->input('amount')
+    );
+
+    return response()->json([
+        'msg' => 'success',
+        'amount' => $newAmount,
+    ]);
 });
